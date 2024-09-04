d7b6888c78b0995018c249cfe084335db708bef5
Rajnish5821Kumar <157160330+Rajnish5821Kumar@users.noreply.github.com>
2024-09-04 12:30:57 +0530

    Task1-App

---
+++ 1.js
@@ -0,0 +1,8 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    if (username === '' || password === '') {
+        alert('All fields are required!');
+        event.preventDefault();
+    }
+});
